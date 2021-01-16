c68ea6ff1bff1b966bc3fd1510533e8678e3d6d9
Christian Maioli M <mmchristian@gmail.com>
2021-01-16 14:49:50 -0300

    Added caching

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Add bot by going to https://discord.com/api/oauth2/authorize?client_id=798538562253488128&permissions=3072&scope=bot
+
 const Discord = require("discord.js");
 require('dotenv').config();
 
@@ -7,8 +9,17 @@ const agent = require('superagent-promise')(request, Promise);
 
 const wtf = require('wtf_wikipedia');
 
+const NodeCache = require( "node-cache" );
+const cache = new NodeCache();
+
 const client = new Discord.Client();
 const baseurl = 'https://liquipedia.net/starcraft/api.php';
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+} 
 
 client.login(process.env.BOT_TOKEN);
 
@@ -18,7 +29,11 @@ client.on('message', message => {
     (async (page, message) => {
       try {
         const res = await request.get(baseurl + '?action=query&prop=revisions&rvprop=content&format=json&titles='+page+'&rvsection=0');
-        const wtxt = Object.values(res.body.query.pages).pop().revisions.pop()["*"];
+        let wtxt = cache.get(page);
+        if ( wtxt == undefined ){
+          wtxt = Object.values(res.body.query.pages).pop().revisions.pop()["*"];
+          cache.set( page, wtxt, 21600 );
+        }
         const doc = wtf(wtxt);
 
         const pic = doc.images(0).json().thumb;
@@ -29,8 +44,9 @@ client.on('message', message => {
 
         const embed = new Discord.MessageEmbed()
           .setThumbnail(pic.replace('https://wikipedia.org/wiki/', 'https://liquipedia.net/starcraft/'))
-          .setDescription('```' + text + '```');
+          .setDescription(text);
         await message.channel.send(embed);
+        await sleep(2);
       } catch (err) {
         console.error(err);
       }
