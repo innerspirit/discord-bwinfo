b968639f1f2f0e158fbf09063a7bda8bd2aa28b1
Christian Maioli M <mmchristian@gmail.com>
2021-01-16 13:53:11 -0300

    Showing basic unit info

--- index.js
+++ index.js
@@ -5,23 +5,35 @@ const request = require('superagent');
 const Promise = this.Promise || require('promise');
 const agent = require('superagent-promise')(request, Promise);
 
+const wtf = require('wtf_wikipedia');
+
 const client = new Discord.Client();
 const baseurl = 'https://liquipedia.net/starcraft/api.php';
 
 client.login(process.env.BOT_TOKEN);
 
 client.on('message', message => {
-  if (message.content === '!' + "marine") {
-    (async () => {
+  if (message.content.startsWith('!bw')) {
+    const page = message.content.slice(4);
+    (async (page, message) => {
       try {
-        const res = await request.get(baseurl + '?action=query&prop=revisions&rvprop=content&format=json&titles=Marine&rvsection=0');
-        console.log(res.body.query.pages[1103].revisions[0]["*"]);
-        // const embed = new Discord.RichEmbed()
-        //   .setDescription(query.pages[1103].revisions[0]["*"]);
-        // message.channel.send(embed);
+        const res = await request.get(baseurl + '?action=query&prop=revisions&rvprop=content&format=json&titles='+page+'&rvsection=0');
+        const wtxt = Object.values(res.body.query.pages).pop().revisions.pop()["*"];
+        const doc = wtf(wtxt);
+
+        const pic = doc.images(0).json().thumb;
+
+        const ib = doc.infobox();
+        const kv = ib.keyValue(); 
+        const text = Object.keys(kv).filter(v => !v.startsWith('bonus') && !v.startsWith('image')).map(v => v + ': ' + kv[v].replace('.png', '') + '\n').join('');
+
+        const embed = new Discord.MessageEmbed()
+          .setThumbnail(pic.replace('https://wikipedia.org/wiki/', 'https://liquipedia.net/starcraft/'))
+          .setDescription('```' + text + '```');
+        await message.channel.send(embed);
       } catch (err) {
         console.error(err);
       }
-    })();
+    })(page, message);
   }
 });
